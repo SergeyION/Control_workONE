5b86816b6315e859d67a0c40a91221fef01c1630
Sergey Ionov <captor_72@bk.ru>
2022-12-05 19:31:52 +0700

    Сохраняем готовую блок-схему

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] array = new string[7];
+
+
+array[0] = "qwe";
+array[1] = "rtyu";
+array[2] = "iopas";
+array[3] = "dfg";
+array[4] = "hjkl";
+array[5] = "zxc";
+array[6] = "vbnm";
+
+Console.WriteLine(array[7]);
+
