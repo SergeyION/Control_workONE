ea9cbb9572b0d0713c1168ad2442337efadfa464
Sergey Ionov <captor_72@bk.ru>
2022-12-10 23:09:53 +0700

    Закончил написание кода

--- Program.cs
+++ Program.cs
@@ -1,18 +1,29 @@
-﻿string[] semiFinal = { "Netherlands", "Argentina", "France", "England", "Croatia", "Spain", "Brazil", "Switzerland" };
-// string[] FinaleFootball = semiFinal.Length;
-
-
-// Метод который перебирает первичный массив и взвращает result
-
+﻿string[] arr = { "Neth", "Arg", "France", "Eng", "Croa", "Spain", "Braz", "Swit" };
+// string[] result = [arr.Length];
 
 // Напишу метод который будет выводить первичный массив в консоль
-void PrintArray(string[] array) // Первичный массив строк
+
+void PrintArray(string[] result) // Первичный массив строк
 {
-    int count = semiFinal.Length; // Переменная равна длинне первичного массива
+    int count = result.Length; // Переменная равна длинне первичного массива
     for (int i = 0; i < count; i++) // Цикл прохода по всем элементам первичного массива
     {
-        Console.Write($"{array[i]} "); // Вывод в консоль
+        Console.Write($"{result[i]} "); // Вывод в консоль
     }
     Console.WriteLine();
 }
-PrintArray(semiFinal); // Проверка работы метода
+
+// Напишем метод который будет возвращать отсортированный массив
+
+string[] SortArray(string[] array)
+{
+    string[] result = new string[array.Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3) result[i] = array[i];
+        
+    }
+    return result;
+}   
+
+PrintArray(SortArray(arr)); // Проверка работы метода
